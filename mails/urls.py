d05eb5cbea23a15ebb03f4d27146669249ebf964
Abhishek <abhishek3188@gmail.com>
2013-09-29 16:30:17 +0530

    Integrated contacts and provider id in views

--- mails/urls.py
+++ mails/urls.py
@@ -6,7 +6,7 @@ from .views import home,index, sign_in, inbox, compose, attachments
 urlpatterns = patterns('',
     url(r"^home/$", home, name="home"),
     url(r"^sign_in/$", sign_in),
-    url(r"^inbox/$", inbox),
+    url(r"^inbox/(?P<provider_id>\w+)?$", inbox),
     url(r"^compose/(?P<provider_id>\w+)?$", compose),
     url(r"^attachments/$", attachments),
 )
