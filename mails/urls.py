0c71df4bea6a3b485f094b3115b8e32157ff993c
Abhishek <abhishek3188@gmail.com>
2013-09-29 11:56:12 +0530

    Rendered template for at view

--- mails/urls.py
+++ mails/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 
-from .views import home,index, sign_in, inbox, compose
+from .views import home,index, sign_in, inbox, compose, attachments
 
 urlpatterns = patterns('',
     url(r"^home/$", home, name="home"),
     url(r"^sign_in/$", sign_in),
     url(r"^inbox/$", inbox),
     url(r"^compose/$", compose),
+    url(r"^attachments/$", attachments),
 )
 
