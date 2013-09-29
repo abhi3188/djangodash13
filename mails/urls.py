0e2c8f4ac197340f18b47f9e4f70bd9ac8f8b3e0
Abhishek <abhishek3188@gmail.com>
2013-09-29 23:12:07 +0530

    Categorization template added

--- mails/urls.py
+++ mails/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 
-from .views import home,index, sign_in, inbox, compose, attachments
+from .views import home,index, sign_in, inbox, compose, attachments, categorize
 
 urlpatterns = patterns('',
     url(r"^home/$", home, name="home"),
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r"^inbox/(?P<provider_id>\w+)?$", inbox, name="inbox"),
     url(r"^compose/(?P<provider_id>\w+)?$", compose),
     url(r"^attachments/(?P<provider_id>\w+)?$", attachments, name="attachments"),
+    url(r"^categorize/$", categorize),
 )
 
